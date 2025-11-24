9e64a2a23ba59c53bc61f6e1c7bd490223e72d90
aliya mahaboob <mahaboobaliya02@gmail.com>
2025-11-24 15:18:10 +0530

    rendering menu items

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
 import { menuArray } from "./data.js";
 
+
+const menuDiv = document.getElementById("menu-item-info");
+
+
+function renderMenu() {
+    let menuHtml = "";
+    menuArray.forEach((menuItem) => {
+        menuHtml += `<div class="menu-item">
+           <div class="items">
+                <img src="${menuItem.image}" class="menu-item-image" />
+                <div class="menu-items-details">
+                   <h3 class="menu-item-name">${menuItem.name}</h3>
+                   <p class="menu-item-ingredients">${menuItem.ingredients}</p>
+                   <p class="menu-item-price">$${menuItem.price}</p>
+                </div>
+           </div>
+            <button class="add-btn" data-menu-id="${menuItem.id}">+</button>
+        </div>
+        <hr>
+        `;
+    })
+    menuDiv.innerHTML = menuHtml;
+}
+ 
+renderMenu();
